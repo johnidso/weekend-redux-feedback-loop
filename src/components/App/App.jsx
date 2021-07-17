58dae7f3b439ddea90c28646ffea613ad11f66d3
John Idso <john.d.idso@gmail.com>
2021-07-17 11:11:27 -0500

    added basic routes and file structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
+import { HashRouter as Router, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
@@ -10,6 +12,26 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Router>
+        <Route path='/' exact>
+          {/* HowFeeling */}
+        </Route>
+        <Route path='/understanding'>
+          {/* HowUnderstanding */}
+        </Route>
+        <Route path='/supported'>
+          {/* HowSupported */}
+        </Route>
+        <Route path='/comments'>
+          {/* Comments */}
+        </Route>
+        <Route path='/review'>
+          {/* Review */}
+        </Route>
+        <Route path='/thankYou'>
+          {/* ThankYou */}
+        </Route>
+      </Router>
     </div>
   );
 }
